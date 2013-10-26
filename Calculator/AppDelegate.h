87f65cd45f257a7b78419532cba97e00cb0a564e
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2013-10-25 23:29:06 -0700

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Developer on 10/25/13.
+//  Copyright (c) 2013 Adam Zaninovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Developer on 10/25/13.
+//  Copyright (c) 2013 Adam Zaninovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
